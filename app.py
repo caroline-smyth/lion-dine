72367f286339ba3157b81eac00bbf2a0e1f77a91
otenwerry <otenwerry@gmail.com>
2024-08-20 19:20:49 -0700

    owen test commit

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import random
 
 app = Flask(__name__)
 @app.route('/')
-
+# owen testing if committing works
 # this does the scraping and converts what is scraped into variables that can be displayed using HTML/CSS/Js
 # Barnard pages need to be scraped separately. Those sites are shit, so need to figure out sm else 
 
