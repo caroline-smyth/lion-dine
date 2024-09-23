4b1961d36d207874d24124ead8d9ded592212046
otenwerry <otenwerry@gmail.com>
2024-09-23 15:58:49 -0400

    more for heroku

--- app.py
+++ app.py
@@ -63,18 +63,17 @@ def managed_webdriver():
     chrome_options.binary_location = chrome_binary
   else:
     raise Exception(f"Unsupported OS: {current_os}")'''
-  chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN', '/app/.apt/usr/bin/google-chrome')
+  chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
   
   chrome_options.add_argument("--headless")
   chrome_options.add_argument("--no-sandbox")
-  #chrome_options.add_argument("--disable-dev-shm-usage")
+  chrome_options.add_argument("--disable-dev-shm-usage")
   chrome_options.add_argument("--disable-gpu")
-  #chrome_options.add_argument("--disable-setuid-sandbox")
-  #chrome_options.add_argument("--remote-debugging-port=9222")
-  #chrome_options.add_argument("--window-size=1920,1080")
+  chrome_options.add_argument("--disable-setuid-sandbox")
+  chrome_options.add_argument("--remote-debugging-port=9222")
+  chrome_options.add_argument("--window-size=1920,1080")
   #service = ChromeService(ChromeDriverManager().install())
-  chrome_driver_path = os.environ.get('CHROMEDRIVER_PATH', '/app/.chromedriver/bin/chromedriver')
-  service = Service(chrome_driver_path)
+  service = Service(executable_path=os.environ.get("CHROMEDRIVER_PATH"))
   driver = webdriver.Chrome(service=service,options=chrome_options)
   try:
     yield driver
