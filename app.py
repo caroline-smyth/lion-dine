3279ed34172d2c8d6799c6c12fe08881cba6fc1d
otenwerry <otenwerry@gmail.com>
2024-09-23 19:53:36 -0400

    oops

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def managed_webdriver():
   driver = webdriver.Chrome(service=service,options=chrome_options)'''
 
   #kill myself
-  chromedriver_path = '/path/to/chromedriver'
+  chromedriver_path = '/usr/bin/chromedriver'
   chrome_options = Options()
   chrome_options.add_argument("--headless")
   chrome_options.add_argument("--no-sandbox")
