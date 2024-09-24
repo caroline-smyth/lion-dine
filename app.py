37bd8b26ae1ee7031dd8258bbb5504a21dfd3d72
otenwerry <otenwerry@gmail.com>
2024-09-24 00:03:15 -0400

    another try

--- app.py
+++ app.py
@@ -83,6 +83,7 @@ def managed_webdriver():
   chrome_options.add_argument("--headless")
   chrome_options.add_argument("--no-sandbox")
   chrome_options.add_argument("--disable-dev-shm-usage")
+  chrome_options.binary_location = '/usr/bin/google-chrome'
   service = Service(chromedriver_path)
   driver = webdriver.Chrome(service=service, options=chrome_options)
 
