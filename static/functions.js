84b1892903c2685c8904d24da509b658e3b519cc
Caroline Smyth <caroline.smyth@gmail.com>
2024-10-02 13:40:26 -0400

    change time format

--- static/functions.js
+++ static/functions.js
@@ -1,9 +1,9 @@
 function updateTime() {
   var now = new Date();
   var options = { 
-      weekday: 'long', 
+      weekday: 'short', 
       year: 'numeric', 
-      month: 'short', 
+      month: 'long', 
       day: 'numeric', 
       hour: '2-digit', 
       minute: '2-digit', 
