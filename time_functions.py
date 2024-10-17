760b5eff303e5a57c42fef7ab7e85abe8ad5bb54
Caroline Smyth <caroline.smyth@gmail.com>
2024-10-17 00:32:32 -0400

    parameterizing time_functions with app.py time

--- time_functions.py
+++ time_functions.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 import pytz
 
-ny_tz = pytz.timezone('America/New_York')
-now = datetime.now(ny_tz)
-weekday = now.weekday()
+#ny_tz = pytz.timezone('America/New_York')
+#now = datetime.now(ny_tz)
+#weekday = now.weekday()
 
-def hours_dict():
+def hours_dict(weekday):
   hours = {}
   # sun - thurs
   if weekday in [0, 1, 2, 3, 6]:
@@ -62,7 +62,7 @@ def hours_dict():
 
   return hours
 
-def breakfast_hours():
+def breakfast_hours(weekday):
   b_hours = {}
 
   b_hours["JJ's"] = "12:00 AM to 10:00 AM"
@@ -86,7 +86,7 @@ def breakfast_hours():
 
   return b_hours
 
-def lunch_hours():
+def lunch_hours(weekday):
   l_hours = {}
 
   l_hours["JJ's"] = "12:00 PM to midnight"
@@ -131,7 +131,7 @@ def lunch_hours():
 
   return l_hours
 
-def dinner_hours():
+def dinner_hours(weekday):
   d_hours = {}
 
   d_hours["JJ's"] = "12:00 PM to midnight"
@@ -172,7 +172,7 @@ def dinner_hours():
 
   return d_hours
 
-def latenight_hours():
+def latenight_hours(weekday):
   ln_hours = {}
   ln_hours["Ferris"] = ln_hours["Faculty House"] = ln_hours["Chef Mike's"] = ln_hours["Chef Don's"] = ln_hours["John Jay"] = ln_hours["Hewitt Dining"] = ln_hours["Grace Dodge"] = "Closed for late night"
   ln_hours["JJ's"] = "Midnight to 10:00 AM"
@@ -189,61 +189,61 @@ def latenight_hours():
   
   return ln_hours
 
-def john_jay_open():
+def john_jay_open(now):
   if now.weekday() in [4,5] or now.hour < 9 or now.hour >= 21 or (now.hour == 9 and now.minute < 30):
     return False
   else:
     return True
   
-def jjs_open():
+def jjs_open(now):
   if now.hour in [10,11]:
     return False
   else:
     return True
 
-def ferris_open():
+def ferris_open(now):
   if ((now.weekday() in [0,1,2,3,4] and (now.hour < 7 or now.hour >= 20 or (now.hour == 7 and now.minute < 30))) or (now.weekday() == 5 and (now.hour < 9 or now.hour >= 20)) or (now.weekday() == 6 and (now.hour < 10 or now.hour >= 20 or now.hour in [14,15]))):
     return False
   else:
     return True
 
-def fac_house_open():
+def fac_house_open(now):
   if now.weekday() > 2 or now.hour < 11 or now.hour > 15 or (now.hour == 14 and now.minute > 30):
     return False
   else:
     return True
 
-def mikes_open():
+def mikes_open(now):
   if now.weekday() in [5,6] or now.hour < 10 or now.hour >= 22 or (now.hour == 10 and now.minute < 30):
     return False
   else:
     return True
 
-def dons_open():
+def dons_open(now):
   if now.weekday() in [5,6] or now.hour < 8 or now.hour >= 18:
     return False
   else:
     return True
 
-def grace_dodge_open():
+def grace_dodge_open(now):
   if now.weekday() in [4,5,6] or now.hour < 11 or now.hour >= 19:
     return False
   else:
     return True
 
-def fac_shack_open():
+def fac_shack_open(now):
   if ((now.weekday() == 6) or (now.weekday() in [0,1,2] and (now.hour < 11 or now.hour >= 14) or (now.weekday() in [4,5] and (now.hour < 19 or now.hour >= 23)) or (now.weekday() == 3 and (now.hour < 11 or now.hour >= 23 or now.hour in [14,15,16,17,18])))):
     return False
   else:
     return True
 
-def hewitt_open():
+def hewitt_open(now):
   if ((now.weekday() in [0,1,2,3,4] and now.hour < 7 or (now.hour == 7 and now.minute < 30) or now.hour == 10 or (now.hour == 14 and now.minute > 30) or now.hour == 15 or (now.hour == 16 and now.minute < 30) or now.hour >= 20) or (now.weekday() in [5,6] and now.hour < 10 or (now.hour == 10 and now.minute < 30) or now.hour == 15 or (now.hour == 16 and now.minute < 30) or now.hour >= 20)):
     return False
   else:
     return True
 
-def diana_open():
+def diana_open(now):
   if (((now.weekday() in [0,1,2,3] and (now.hour < 9 or now.hour in [15,16])) or 
        (now.weekday() == 4 and (now.hour < 9 or now.hour >= 15)) or 
        (now.weekday() == 5) or 
