188fb02f65545c0990e05475b40446a8ca1b743c
otenwerry <otenwerry@gmail.com>
2025-02-16 20:22:28 +0000

    makes sure the database has been created

--- app.py
+++ app.py
@@ -725,4 +725,6 @@ def swipemarket():
   return render_template('swipemarket.html')
 
 if __name__ == '__main__':
+   with app.app_context():
+     db.create_all()
    app.run(host='0.0.0.0',port=5000)
