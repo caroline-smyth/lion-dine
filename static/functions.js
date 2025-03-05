26462505d54dc00291e04c66b4c7daafa53e2e45
Caroline Smyth <caroline.smyth@gmail.com>
2025-03-05 14:41:59 +0100

    replacing deprecated login

--- static/functions.js
+++ static/functions.js
@@ -9,14 +9,6 @@ function onSignIn(googleUser) {
 
 }
 
-//google sign out function
-function signOut() {
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function () {
-    console.log('User signed out.');
-  });
-}
-
 function updateTime() {
   var now = new Date();
   var options = { 
