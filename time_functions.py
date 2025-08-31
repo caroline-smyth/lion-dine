4305acea84c37a92095cab22740c753b8e883900
Caroline Smyth <caroline.smyth@gmail.com>
2025-08-31 10:43:04 -0400

    space sunday

--- time_functions.py
+++ time_functions.py
@@ -8,9 +8,9 @@ def nsop_hours(weekday, now):
     
     for hall in all_halls:
         hours[hall] = "Closed for summer"
-    hours["John Jay"] = "9:30AM to 9:00PM"
+    hours["John Jay"] = "9:30 AM to 9:00 PM"
     hours["Ferris"] = "9:00 AM to 8:00 PM"
-    hours["Johnny's"] = "11:00 AM to 2:30 PM"
+    hours["Chef Mike's"] = "11:00 AM to 8:00 PM"
     hours["JJ's"] = "12:00 PM to 8:00 PM"
 
     
