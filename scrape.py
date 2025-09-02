fabeeeca1ddb2192ab779771e17f281c39083a0f
Caroline Smyth <caroline.smyth@gmail.com>
2025-09-02 11:35:17 -0400

    john jay pasta quesa

--- scrape.py
+++ scrape.py
@@ -185,6 +185,10 @@ def scrape_columbia(hall_name):
               meal_items_text = [item.text.strip() for item in meal_items]
           elif "Pasta/Quesadilla/Rice" in station_name:
             station_name = "Pasta, Quesadilla & Rice Bowl Station"
+
+        if "john jay" in actual_name:
+          if "Pasta/Quesadilla/Rice" in station_name:
+            station_name = "Pasta, Quesadilla & Rice Bowl Station"
         if "chef mike's" in actual_name:
           meal_descriptions = s.find_elements(By.CLASS_NAME, "meal-description")
           meal_descriptions_text = [desc.text.strip() for desc in meal_descriptions]
